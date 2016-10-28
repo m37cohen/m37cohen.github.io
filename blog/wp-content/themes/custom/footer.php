171bd64b5479ba505f1ac9ee239d89e9da4ca5f4
YOUR NAME <YOUR@EMAIL.com>
2016-10-28 11:49:37 -0400

    update
    update

---
+++ blog/wp-content/themes/custom/footer.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
+?>
+
+		</div><!-- .site-content -->
+	</div><!-- .site-inner -->
+</div><!-- .site -->
+
+        <!-- Footer
+    ================================================== -->
+    <div class="block-footer">
+        <div class="container-fluid footer-container">
+            <div class="content-left footer-left">
+                <div class="row">
+                    <a href="http://www.ryerson.ca/index.html" target="blank"><img src="http://localhost:8888/sdtoronto/wp-content/uploads/2016/10/ryerson.png" class="footer-logo"></a>
+                    <a href="https://www.utoronto.ca/" target="blank"><img src="http://localhost:8888/sdtoronto/wp-content/uploads/2016/10/uoft.png" class="footer-logo"></a>
+                    <a href="http://www.senecacollege.ca/" target="blank"><img src="http://localhost:8888/sdtoronto/wp-content/uploads/2016/10/seneca.png" class="footer-logo"></a>
+                </div>
+            </div>
+            <div class="content-right footer-right">
+                <div class="row">
+                    <ul class="list-inline social">
+                        <li><a href="http://facebook.com/sdteamtoronto" target="blank"><i class="fa icon-color fa-facebook"></i></a></li>
+                        <li><a href="http://twitter.com/sdteamto" target="blank"><i class="fa icon-color fa-twitter"></i></a></li>
+                        <li><a href="http://instagram.com/sdteamtoronto" target="blank"><i class="fa icon-color fa-instagram"></i></a></li>
+                        <li><a href="https://www.linkedin.com/company/sd-team-toronto" target="blank"><i class="fa icon-color fa-linkedin"></i></a></li>
+                        <li><a href="contact.html"><i class="fa icon-color fa-envelope"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ blog/wp-content/themes/custom/header.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * The template for displaying the header
+ *
+ * Displays all of the head element and everything up until the "site-content" div.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
+
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js">
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<?php endif; ?>
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+		
+
+				<!-- Navigation 
+			    ================================================== -->
+
+			    <div class="block-nav">
+			        <div class="container-fluid nav-container">
+			            <div class="nav-left">
+			                <a class="nav-logo" href="index.html"><img src="http://localhost:8888/sdtoronto/wp-content/uploads/2016/10/logo.png"></a>
+			            </div>
+			            <div class="nav-right">
+			                <div class="" id="">
+			                  <ul class="nav navbar-nav">
+			                    <li><a href="about.html" class="">About</a></li>
+			                    <li class="dropdown">
+			                        <a href="concept.html" class="dropdown-btn">Concept</a>
+			                        <div class="dropdown-content">
+			                          <a href="architecture.html">Architecture</a>
+			                          <a href="buildingscience.html">Building Science</a>
+			                          <a href="electrical.html">Electrical</a>
+			                          <a href="interiordesign.html">Interior Design</a>
+			                          <a href="mechanical.html">Mechanical</a>
+			                        </div>
+			                    </li>
+			                    <li><a href="team.html" class="">Team</a></li>
+			                    <li><a href="support.html" class="">Support</a></li>
+			                    <li><a href="blog.html" class="">News</a></li>
+			                    <li><a href="contact.html" class="">Contact</a></li>
+			                  </ul>
+			                </div>
+			            </div>
+			        </div>
+			    </div>
+
+				<?php if ( has_nav_menu( 'primary' ) || has_nav_menu( 'social' ) ) : ?>
+					<button id="menu-toggle" class="menu-toggle"><?php _e( 'Menu', 'twentysixteen' ); ?></button>
+
+					<div id="site-header-menu" class="site-header-menu">
+						<?php if ( has_nav_menu( 'primary' ) ) : ?>
+							<nav id="site-navigation" class="main-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Primary Menu', 'twentysixteen' ); ?>">
+								<?php
+									wp_nav_menu( array(
+										'theme_location' => 'primary',
+										'menu_class'     => 'primary-menu',
+									 ) );
+								?>
+							</nav><!-- .main-navigation -->
+						<?php endif; ?>
+
+						<?php if ( has_nav_menu( 'social' ) ) : ?>
+							<nav id="social-navigation" class="social-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Social Links Menu', 'twentysixteen' ); ?>">
+								<?php
+									wp_nav_menu( array(
+										'theme_location' => 'social',
+										'menu_class'     => 'social-links-menu',
+										'depth'          => 1,
+										'link_before'    => '<span class="screen-reader-text">',
+										'link_after'     => '</span>',
+									) );
+								?>
+							</nav><!-- .social-navigation -->
+						<?php endif; ?>
+					</div><!-- .site-header-menu -->
+				<?php endif; ?>
+			</div><!-- .site-header-main -->
+
+			<?php if ( get_header_image() ) : ?>
+				<?php
+					/**
+					 * Filter the default twentysixteen custom header sizes attribute.
+					 *
+					 * @since Twenty Sixteen 1.0
+					 *
+					 * @param string $custom_header_sizes sizes attribute
+					 * for Custom Header. Default '(max-width: 709px) 85vw,
+					 * (max-width: 909px) 81vw, (max-width: 1362px) 88vw, 1200px'.
+					 */
+					$custom_header_sizes = apply_filters( 'twentysixteen_custom_header_sizes', '(max-width: 709px) 85vw, (max-width: 909px) 81vw, (max-width: 1362px) 88vw, 1200px' );
+				?>
+				<div class="header-image">
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+						<img src="<?php header_image(); ?>" srcset="<?php echo esc_attr( wp_get_attachment_image_srcset( get_custom_header()->attachment_id ) ); ?>" sizes="<?php echo esc_attr( $custom_header_sizes ); ?>" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
+					</a>
+				</div><!-- .header-image -->
+			<?php endif; // End header image check. ?>
+
+
+		<div class="site-wrap">
+			<div class="container-fluid" style="background-color:#fff">
